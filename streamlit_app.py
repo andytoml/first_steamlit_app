15abcd33460cf55e95e6931ebdb83dd7208c9964
Andy Tomlinson <117351357+andytoml@users.noreply.github.com>
2022-11-07 09:49:49 +0000

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -22,9 +22,9 @@ streamlit.header('🍌🍓Build Your Own Fruit Smoothie🍏🍇')
 # Let's put a pick list here so they can pick the fruit they want to include 
 fruits_selected = streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index),['Avocado','Strawberries'])
 fruits_to_show = my_fruit_list.loc[fruits_selected]
-
 # Display the table on the page.
 streamlit.dataframe(fruits_to_show)
+
 #New Section to display fruityvice API response
 streamlit.header("Fruityvice Fruit Advice!")
 try:
@@ -32,7 +32,7 @@ try:
     if not fruit_choice:
        streamlit.error("Please select a fruit to get information.")
     else:
-      fruitvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
+      fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
       fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
       streamlit.dataframe(fruityvice_normalized)
 
